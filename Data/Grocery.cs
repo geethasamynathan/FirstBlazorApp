102ee6e38ce507afe827ae6332a31800ea8bf49e
geethasamynathan <geethasamynathan2011@gmail.com>
2024-04-29 10:45:08 +0530

    Add project files.

---
+++ Data/Grocery.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstBlazorApp.Data
+{
+    public class Grocery
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+}
---
+++ Data/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstBlazorApp.Data
+{
+    public class User
+    {
+
+      
+        public string username { get; set; }
+      
+        public string email { get; set; }
+    }
+}
